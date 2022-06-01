a46cb42f6a51af00f4546643618bc8e376330285
Seyi <kuforiji98@gmail.com>
2022-06-01 23:59:07 +0100

    workdone

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for letters in range(ord('a'), ord('z')):
-	print("{:c}".format(letters), end ="")
+    print("{:c}".format(letters), end ="")
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def is_lower(c):
-	if ord(c) >= (ord('a') and ord('z')):
-		return TRUE
-	else:
-		return FALSE
+    if ord(c) >= (ord('a') and ord('z')):
+        return TRUE
+    else:
+        return FALSE
