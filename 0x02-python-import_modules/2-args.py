2912ea9b39982d52bf0e2fe5704c440e5fd770a3
Seyi <kuforiji98@gmail.com>
2022-06-02 14:51:39 +0100

    initial workdone

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    num = len(sys.argv)
+    if num == 1:
+        print("{} argument.".format(num - 1))
+    else:
+        print("{} argument:".format(num - 1))
+
+    for args in range(1, num):
+        print("{}: {}".format(args, sys.argv[args]))
