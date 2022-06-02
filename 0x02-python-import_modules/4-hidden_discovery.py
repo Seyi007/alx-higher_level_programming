a170b110a098946950741f4adfd945c7700db14c
Seyi <kuforiji98@gmail.com>
2022-06-02 16:10:24 +0100

    initial workdone

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     from hidden_4 import *
     items = dir()
     for i in range(0, len(items)):
-        if arr[i][0:2] != "__":
+        if items[i][0:2] != "__":
             print("{}".format(items[i]))
