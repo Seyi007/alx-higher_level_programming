4b128cf4459cc9d1f68c62ce5e04f4c31693abc7
Seyi <kuforiji98@gmail.com>
2022-06-02 16:04:26 +0100

    initial workdone

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
+
 #!/usr/bin/python3
 if __name__ == "__main__":
     from hidden_4 import *
     items = dir()
     for i in range(0, len(items)):
-        print("{}".format(sort(items)))
+        print("{}".format(items[i]))
